d5b6d78e243b601c49ef5aa7e141c51db6cf0c1f
LucaSforza <lucasforza1234@icloud.com>
2023-07-26 02:23:56 +0200

    Now the interpreter first compiles the code into bytecode, optimizes it, and then executes it

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
-use std::{fs::File, io::Read, path::Path};
+use std::{
+    fs::File,
+    io::{Bytes, Read},
+    path::Path,
+};
 
 use clap::Parser;
+
+const DEBUG: bool = false;
 
 #[derive(Debug, Parser)]
 #[clap(author, version, about)]
@@ -15,6 +21,107 @@ fn read_input() -> Result<u8, std::io::Error> {
     Ok(input[0])
 }
 
+#[derive(Debug, Clone)]
+enum Token {
+    Output,
+    Input,
+    Move(isize),
+    IncValue(isize),
+    Loop(Vec<Token>),
+}
+
+fn compile(mut bytes: Bytes<File>, level: usize) -> (Vec<Token>, Bytes<File>) {
+    let mut tokens: Vec<Token> = Vec::new();
+
+    loop {
+        if let Some(byte) = bytes.next() {
+            let char = byte.unwrap() as char;
+            match char {
+                '>' => tokens.push(Token::Move(1)),
+                '<' => tokens.push(Token::Move(-1)),
+                '+' => tokens.push(Token::IncValue(1)),
+                '-' => tokens.push(Token::IncValue(-1)),
+                '.' => tokens.push(Token::Output),
+                ',' => tokens.push(Token::Input),
+                '[' => {
+                    let (inner_tokens, next_bytes) = compile(bytes, level + 1);
+                    tokens.push(Token::Loop(inner_tokens));
+                    bytes = next_bytes
+                }
+                ']' => {
+                    if level == 0 {
+                        eprintln!("closed an nonexistent loop");
+                        std::process::exit(1);
+                    } else {
+                        return (tokens, bytes);
+                    }
+                }
+                _ => (),
+            }
+        } else {
+            if level > 0 {
+                eprintln!("opened an nonexistent loop");
+                std::process::exit(1);
+            }
+            break;
+        }
+    }
+
+    (tokens, bytes)
+}
+
+fn optimize(tokens: Vec<Token>) -> Vec<Token> {
+    let mut optimized: Vec<Token> = Vec::new();
+
+    for token in tokens {
+        match (optimized.last(), token) {
+            (_, Token::Loop(sub_exp)) => {
+                optimized.push(Token::Loop(optimize(sub_exp)));
+            }
+
+            (Some(&Token::IncValue(n)), Token::IncValue(v)) => {
+                optimized.pop();
+                optimized.push(Token::IncValue(n + v));
+            }
+
+            (Some(&Token::Move(n)), Token::Move(v)) => {
+                optimized.pop();
+                optimized.push(Token::Move(n + v))
+            }
+
+            (_, e) => optimized.push(e.clone()),
+        }
+    }
+    optimized
+}
+
+fn run(memory: &mut Vec<i8>, dp: &mut isize, ip: &mut isize, tokens: &Vec<Token>) {
+    for token in tokens {
+        if DEBUG {
+            println!("{:?}", *token)
+        }
+        match token {
+            Token::Output => print!("{}", memory[*dp as usize] as u8 as char),
+            Token::Input => {
+                memory[*dp as usize] = match read_input() {
+                    Ok(data) => data as i8,
+                    Err(e) => {
+                        eprintln!("error while reading data: {}", e);
+                        std::process::exit(1);
+                    }
+                }
+            }
+            Token::Move(v) => *dp += v,
+            Token::IncValue(v) => memory[*dp as usize] += *v as i8,
+            Token::Loop(istr) => {
+                while memory[*dp as usize] != 0 {
+                    run(memory, dp, ip, istr)
+                }
+            }
+        }
+    }
+}
+
 fn main() {
     let args = Args::parse();
     let path = Path::new(args.path.as_str());
@@ -23,83 +130,13 @@ fn main() {
         std::process::exit(1);
     }
 
-    let debug = false;
-
     let file = File::open(path).expect("the File can't be open or it is not a file");
 
     let mut memory: Vec<i8> = vec![0; 30_000];
-    let mut dp: usize = 0;
-    let mut ip: usize = 0;
+    let mut dp: isize = 0;
+    let mut ip: isize = 0;
 
-    let bytes: Vec<u8> = file.bytes().map(|b| b.unwrap()).collect();
+    let tokens = optimize(compile(file.bytes(), 0).0);
 
-    loop {
-        if ip >= bytes.len() {
-            break;
-        }
-        let byte = bytes[ip] as char;
-
-        if debug {
-            println!("istruction: {}", byte);
-            println!("data ptr: {}", dp);
-            memory.iter().enumerate().for_each(|(i, v)| {
-                if *v != 0 {
-                    println!("{}: {}", i, v)
-                }
-            })
-        }
-
-        match byte {
-            '>' => dp += 1,
-            '<' => dp -= 1,
-            '+' => memory[dp] += 1,
-            '-' => memory[dp] -= 1,
-            '.' => print!("{}", memory[dp] as u8 as char),
-            '[' => {
-                if memory[dp] == 0 {
-                    let mut skip: usize = 0;
-                    loop {
-                        ip += 1;
-                        let r = bytes[ip] as char;
-                        if r == ']' {
-                            if skip == 0 {
-                                break;
-                            }
-                            skip -= 1
-                        } else if r == '[' {
-                            skip += 1
-                        }
-                    }
-                }
-            }
-            ']' => {
-                if memory[dp] != 0 {
-                    let mut skip: usize = 0;
-                    loop {
-                        ip -= 1;
-                        let r = bytes[ip] as char;
-                        if r == '[' {
-                            if skip == 0 {
-                                break;
-                            }
-                            skip -= 1
-                        } else if r == ']' {
-                            skip += 1
-                        }
-                    }
-                }
-            }
-            ',' => {
-                memory[dp] = match read_input() {
-                    Ok(data) => data as i8,
-                    Err(e) => {
-                        eprintln!("error while reading data: {}", e);
-                        std::process::exit(1);
-                    }
-                }
-            }
-            _ => {}
-        }
-        ip += 1;
-    }
+    run(&mut memory, &mut dp, &mut ip, &tokens)
 }
